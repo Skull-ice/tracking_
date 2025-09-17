8f10ecbda32f9037732223074c9ec2562e2cc871
Edson Vigninou <edsonpaulinvigninou@gmail.com>
2025-09-18 00:59:47 +0100

    Update tracking_test.py

--- tracking_test.py
+++ tracking_test.py
@@ -1,7 +1,8 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-# Injecter le code GTM dans le "head" (le plus haut possible)
-st.markdown("""
+# Injecter GTM dans le "head" (le plus haut possible)
+components.html("""
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -9,15 +10,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-57BGF9ZN');</script>
 <!-- End Google Tag Manager -->
-""", unsafe_allow_html=True)
+""", height=0)
 
-# Injecter le code GTM noscript juste après (simule début du body)
-st.markdown("""
+# Injecter noscript dans le "body"
+components.html("""
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57BGF9ZN"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-""", unsafe_allow_html=True)
+""", height=0)
 
 # Titre
 st.title("Test de Tracking GTM")
@@ -34,8 +35,8 @@ Vérifiez les événements dans :
 # Bouton pour tester le tracking
 if st.button("Tester le Tracking"):
     st.success("Événement 'test_click' envoyé !")
-    st.markdown("""
+    components.html("""
     <script>
     dataLayer.push({'event': 'test_click', 'action': 'button_clicked'});
     </script>
-    """, unsafe_allow_html=True)
+    """, height=0)
